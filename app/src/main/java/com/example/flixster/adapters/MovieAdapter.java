2144f27a4080a4b99f3c4e60c1967be26b8281d5
nishitha1 <nssuvarn@uci.edu>
2021-01-19 09:54:46 -0800

    Flixter 2 app changes

--- app/src/main/java/com/example/flixster/adapters/MovieAdapter.java
+++ app/src/main/java/com/example/flixster/adapters/MovieAdapter.java
@@ -1,24 +1,40 @@
 package com.example.flixster.adapters;
 
 import android.content.Context;
+import android.content.Intent;
 import android.content.res.Configuration;
 import android.graphics.Color;
 import android.graphics.drawable.ColorDrawable;
 import android.util.Log;
+import android.util.Pair;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.ImageView;
+import android.widget.RelativeLayout;
 import android.widget.TextView;
 
 import androidx.annotation.NonNull;
+import androidx.core.app.ActivityOptionsCompat;
 import androidx.recyclerview.widget.RecyclerView;
 
 import com.bumptech.glide.Glide;
+import com.bumptech.glide.load.resource.bitmap.CenterCrop;
+import com.bumptech.glide.load.resource.bitmap.RoundedCorners;
+import com.bumptech.glide.request.RequestOptions;
+import com.example.flixster.DetailActivity;
+import com.example.flixster.PopularMoviesActivity;
 import com.example.flixster.R;
 import com.example.flixster.models.Movie;
 
+import org.parceler.Parcels;
+
 import java.util.List;
+
+import jp.wasabeef.glide.transformations.BlurTransformation;
+import jp.wasabeef.glide.transformations.RoundedCornersTransformation;
+
+import static com.bumptech.glide.load.resource.drawable.DrawableTransitionOptions.withCrossFade;
 
 public class MovieAdapter extends RecyclerView.Adapter<MovieAdapter.ViewHolder> {
     Context context;
@@ -28,6 +44,7 @@ public class MovieAdapter extends RecyclerView.Adapter<MovieAdapter.ViewHolder> 
         this.context = context;
         this.movies = movies;
     }
+
     //Usually involves inflating a layout from XML and returning the holder
     @NonNull
     @Override
@@ -53,34 +70,67 @@ public class MovieAdapter extends RecyclerView.Adapter<MovieAdapter.ViewHolder> 
         return movies.size();
     }
 
-    public class ViewHolder extends RecyclerView.ViewHolder{
+    public class ViewHolder extends RecyclerView.ViewHolder {
+
+        RelativeLayout movieContainer;
         TextView tvTitle;
         TextView tvOverview;
         ImageView ivPoster;
-        public ViewHolder(@NonNull View itemView){
+
+        public ViewHolder(@NonNull View itemView) {
             super(itemView);
             tvTitle = itemView.findViewById(R.id.tvTitle);
             tvOverview = itemView.findViewById(R.id.tvOverview);
             ivPoster = itemView.findViewById(R.id.ivPoster);
+            movieContainer = itemView.findViewById(R.id.movieContainer);
         }
 
-        public void bind(Movie movie) {
+        public void bind(final Movie movie) {
             tvTitle.setText(movie.getTitle());
             tvOverview.setText(movie.getOverview());
             String imageUrl;
             // if phone is in landscape
-            if (context.getResources().getConfiguration().orientation == Configuration.ORIENTATION_LANDSCAPE){
+            if (context.getResources().getConfiguration().orientation == Configuration.ORIENTATION_LANDSCAPE) {
                 // then imageUrl = backdrop image
                 imageUrl = movie.getBackdropPath();
-            }else {
+            } else {
                 // else imageUrl = poster image
                 imageUrl = movie.getPosterPath();
             }
-            //Render images use hint
             // added placeholder
-//            Glide.with(context).load(imageUrl).into(ivPoster);
-            Glide.with(context).load(imageUrl).placeholder(R.drawable.loading).into(ivPoster);
-//            Glide.with(context).load(imageUrl).placeholder(new ColorDrawable(Color.TRANSPARENT)).into(ivPoster);
+//            int radius = 5;
+
+//            Glide.with(context).load(imageUrl).transform(new BlurTransformation()).into(ivPoster);
+
+//            Glide.with(context).load(imageUrl).transform(new RoundedCornersTransformation(2, 0)).into(ivPoster);
+//            Glide.with(context).load(imageUrl).apply(new RequestOptions().transform(new RoundedCorners(10))).into(ivPoster);
+            Glide.with(context).load(imageUrl).into(ivPoster);
+
+            //Glide.with(context).load(imageUrl).placeholder(R.drawable.loading).into(ivPoster);
+            //Glide.with(context).load(imageUrl).placeholder(new ColorDrawable(Color.TRANSPARENT)).into(ivPoster)
+
+            //1. Register click listener on the whole row
+            movieContainer.setOnClickListener(new View.OnClickListener() {
+                @Override
+                public void onClick(View v) {
+                    //2. Navigate to a new activity on tap
+                    if ((float) movie.getRating() > 5.0) {
+                        Intent inte = new Intent(context, PopularMoviesActivity.class);
+                        inte.putExtra("movie", Parcels.wrap(movie));
+                        context.startActivity(inte);
+                    } else {
+                        Intent i = new Intent(context, DetailActivity.class);
+                        //Pass using parceler
+                        i.putExtra("movie", Parcels.wrap(movie));
+                        // Shared element transistion
+//                    Pair<View, String> p1 = Pair.create((View) tvTitle, "title");
+//                    Pair<View, String> p2 = Pair.create((View) tvOverview, "overview");
+//                    ActivityOptionsCompat options = ActivityOptionsCompat.
+//                            makeSceneTransitionAnimation(context, p1, p2);
+                        context.startActivity(i);
+                    }
+                }
+            });
         }
     }
 }
--- app/src/main/java/com/example/flixster/models/Movie.java
+++ app/src/main/java/com/example/flixster/models/Movie.java
@@ -3,26 +3,36 @@ package com.example.flixster.models;
 import org.json.JSONArray;
 import org.json.JSONException;
 import org.json.JSONObject;
+import org.parceler.Parcel;
 
 import java.util.ArrayList;
 import java.util.List;
 
+@Parcel
 public class Movie {
     String backdropPath;
     String posterPath;
     String title;
     String overview;
+    double rating;
+    int movieId;
+
+    public Movie() {
+        //Empty constructor needed by the Parceler library
+    }
 
     public Movie(JSONObject jsonObject) throws JSONException {
         backdropPath = jsonObject.getString("backdrop_path");
         posterPath = jsonObject.getString("poster_path");
         title = jsonObject.getString("title");
         overview = jsonObject.getString("overview");
+        rating = jsonObject.getDouble("vote_average");
+        movieId = jsonObject.getInt("id");
     }
 
     public static List<Movie> fromJSONArray(JSONArray movieJsonArray) throws JSONException {
         List<Movie> movies = new ArrayList<>();
-        for (int i = 0; i < movieJsonArray.length(); i++){
+        for (int i = 0; i < movieJsonArray.length(); i++) {
             movies.add(new Movie(movieJsonArray.getJSONObject(i)));
         }
         return movies;
@@ -32,14 +42,24 @@ public class Movie {
         return String.format("https://image.tmdb.org/t/p/w342/%s", posterPath);
     }
 
-    public String getBackdropPath(){
-        return String.format("https://image.tmdb.org/t/p/w342/%s" ,backdropPath);
+    public String getBackdropPath() {
+        return String.format("https://image.tmdb.org/t/p/w342/%s", backdropPath);
     }
+
     public String getTitle() {
         return title;
     }
 
     public String getOverview() {
         return overview;
+    }
+
+
+    public double getRating() {
+        return rating;
+    }
+
+    public int getMovieId() {
+        return movieId;
     }
 }
